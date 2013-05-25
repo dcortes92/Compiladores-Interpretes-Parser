ac7cffca4f67448ed0735350c0cb4bb97fac3895
Isaac <danielramso_12@hotmail.com>
2013-05-25 17:33:21 -0600

    Gramatica para Flex

---
+++ Nodo.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Nodo de la lista*/
+struct nodoLista
+{
+    char *etiqueta; /*etiqueta del nodo*/
+    char *padre; /*padre de la etiqueta, usado para la jerarquía del árbol*/
+    struct nodoLista *ptrSiguiente;
+};
+
+struct nodoLista *nodoInicial = NULL; /*Puntero que tiene la referencia al primer nodo*/
+struct nodoLista *nodoActual = NULL; /*Puntero que tiene la refenrencia al nodo actual,
+										se usa para insertar al final.*/
+
+/*Prototipos de las funciones*/
+struct nodoLista* crearLista(char *tag, char *pad);
+struct nodoLista* insertar(char *tag, char *pad);
+void imprimir(void);
